d5913db8fa2eb12c3efc4daae50af15e1d91fde8
Caleb Rogers <caleb@calebjay.com>
2021-07-20 22:00:04 +0800

    WIP

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,8 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-username = os.getenv('MYSQL_USER')
-password = os.getenv('MYSQL_ROOT_PASSWORD')
-host = os.getenv('MYSQL_HOST')
-port = os.getenv('MYSQL_PORT')
-DB_NAME = os.getenv('MYSQL_DB')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{username}:{password}@{host}:{port}"
-
-db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
---
+++ backend/models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import Date
+from .database import Base
