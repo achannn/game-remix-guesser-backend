78a395e9bb4fff25ec523bdc0e7311ac2867678b
Caleb Rogers <caleb@calebjay.com>
2021-10-21 20:48:03 +0800

    Initial styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,15 +4,35 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+  <div class="grass"></div>
+  <div class="ground"></div>
 </template>
 
 <style lang="scss">
+@import 'colors';
+
+* {
+  font-family: "Press Start 2P";
+}
+body {
+    background-color: $saddleBrown;
+}
+.grass {
+    height: 2vh;
+    background-color: $lawnGreen;
+}
+.ground {
+    height: 2vh;
+    background-color: $darkGreen;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: $lightCornflowerBlue;
+
 }
 
 #nav {
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,26 +1,46 @@
 <template>
   <div class="home">
-    <SubmitOcremixId />
-    <GenerateQuestionButton />
-    <CompareIDs />
+      <section class="introduction nes-container is-rounded">
+          <p>Welcome to the Game Music Guessing Game!</p>
+          <p>To play, click the "Play" button below.</p>
+          <button class="play nes-btn">Play</button>
+          <h2>Instructions</h2>
+          <p>You will be presented with a play button. When you click it, music will start playing,
+              and four video game titles will display as radio items.</p>
+          <p>When you think you know which game the song is derived from,
+              choose it in the list, and click "Submit my Guess"</p>
+          <p>The game will tell you whether you chose properly or not,
+              and show you some more information about the song,
+              such as the Ocremix where you listen to more music by the artist.</p>
+          <p>Then, you can get a new song by clicking "next song".</p>
+          <p>The game runs indefinitely, play as long as you like!</p>
+      </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import SubmitOcremixId from '@/components/SubmitOcremixId.vue';
-import GenerateQuestionButton from '@/components/GenerateQuestionButton.vue';
-import CompareIDs from '@/components/CompareSecretPublicIDs.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    SubmitOcremixId,
-    GenerateQuestionButton,
-    CompareIDs,
   },
   mounted() {
     this.$store.dispatch('getRemixes');
   },
 });
 </script>
+<style lang="scss" scoped>
+
+.home {
+    display: flex;
+    justify-content: center;
+}
+.introduction {
+    width: 80vw;
+    margin-bottom: 18px;
+}
+.play {
+    margin-bottom: 16px;
+}
+</style>
