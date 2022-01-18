e207faba1551dbb8d012d3897a9d8f2b7d7072de
Caleb Rogers <caleb@calebjay.com>
2022-01-18 16:55:28 +0800

    Heroku preparations

--- backend/run.sh
+++ backend/run.sh
@@ -4,10 +4,8 @@ set +a
 
 if [ ${ENV} = "DEV" ]; then
     echo "Running dev environment"
-    export GOOGLE_APPLICATION_CREDENTIALS="game-remix-guesser-a41630194599.json"
     uvicorn app.main:app --reload --host=0.0.0.0 --port=8000
 else
     echo "Running prod environment"
-
-    gunicorn app.main:app -w 4 -k uvicorn.workers.UvicornWorker --bind :8080
+    uvicorn app.main:app --host=0.0.0.0 --port=80
 fi
